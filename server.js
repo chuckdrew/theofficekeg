c94cc77e1d88b384e6e4f82e89cb40e104e72901
Charles Drew <cdrew@directvetmarketing.com>
2015-03-25 19:02:47 -0400

    no message

--- server.js
+++ server.js
@@ -7,9 +7,11 @@ var bodyParser = require('body-parser');
 var morgan = require('morgan');
 var cookieParser = require('cookie-parser');
 var session = require('express-session');
+var MongoStore = require('connect-mongo')(session);
 var configDB = require('./config/database.js');
-mongoose.connect(configDB.url);
 require('./config/passport')(passport);
+
+app.use(express.static(__dirname + '/public'));
 
 express.response.apiRes = function(status, message, data) {
     this.json({
@@ -19,20 +21,37 @@ express.response.apiRes = function(status, message, data) {
     });
 }
 
+//Store Sessions in Mongo
+mongoose.connect(configDB.url);
+
 app.use(morgan('dev')); // log every request to the console
 app.use(cookieParser());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-app.use(session({ secret: 'mysecurekey' }));
+app.use(session({
+    secret: 'mysecurekey',
+    store: new MongoStore({ mongooseConnection: mongoose.connection })
+}));
+
 app.use(passport.initialize());
 app.use(passport.session());
 
 app.use('/', require('./controllers/index')(app, passport));
 app.use('/api', passport.authenticate('basic', { session: false }), require('./controllers/index')(app, passport));
 
-app.get('*', function(req, res) {
+app.get('/', function(req, res) {
     res.sendfile('./public/index.html');
+});
+
+//404
+app.use(function(req, res) {
+    res.send('404: Page not Found', 404);
+});
+
+//500
+app.use(function(error, req, res, next) {
+    res.send('500: Internal Server Error', 500);
 });
 
 app.listen(port);
